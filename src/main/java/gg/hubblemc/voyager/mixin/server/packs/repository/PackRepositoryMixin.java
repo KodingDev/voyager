61a69ddfe99fd40dd3b5aad0355e288316fe2350
Koding Dev <kodingdev@pm.me>
2022-12-17 13:54:06 +1100

    feat: update to 1.19.3

--- src/main/java/gg/hubblemc/voyager/mixin/server/packs/repository/PackRepositoryMixin.java
+++ src/main/java/gg/hubblemc/voyager/mixin/server/packs/repository/PackRepositoryMixin.java
@@ -1,8 +1,8 @@
 package gg.hubblemc.voyager.mixin.server.packs.repository;
 
 import gg.hubblemc.voyager.Voyager;
+import net.minecraft.server.packs.PackType;
 import net.minecraft.server.packs.repository.FolderRepositorySource;
-import net.minecraft.server.packs.repository.Pack;
 import net.minecraft.server.packs.repository.PackRepository;
 import net.minecraft.server.packs.repository.PackSource;
 import net.minecraft.server.packs.repository.RepositorySource;
@@ -15,6 +15,8 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.io.File;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Path;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -26,16 +28,21 @@ public class PackRepositoryMixin {
     @Mutable
     private Set<RepositorySource> sources;
 
-    @Inject(method = "<init>(Lnet/minecraft/server/packs/repository/Pack$PackConstructor;[Lnet/minecraft/server/packs/repository/RepositorySource;)V", at = @At("RETURN"))
-    public void construct(Pack.PackConstructor arg, RepositorySource[] resourcePackProviders, CallbackInfo info) {
+    @Inject(method = "<init>", at = @At("RETURN"))
+    public void construct(RepositorySource[] resourcePackProviders, CallbackInfo info) {
         sources = new HashSet<>(sources);
 
         String property = System.getProperty("voyager.repo-folders");
         if (property == null) return;
 
-        for (String path : property.split(File.pathSeparator)) {
-            Voyager.LOGGER.info("Adding pack repository folder: " + path);
-            sources.add(new FolderRepositorySource(new File(path), PackSource.BUILT_IN));
+        for (String pathStr : property.split(File.pathSeparator)) {
+            try {
+                Path path = Path.of(pathStr);
+                Voyager.LOGGER.info("Adding pack repository folder: " + path);
+                sources.add(new FolderRepositorySource(path, PackType.CLIENT_RESOURCES, PackSource.BUILT_IN));
+            } catch (InvalidPathException e) {
+                Voyager.LOGGER.error("Invalid path for pack repository folder: " + pathStr);
+            }
         }
     }
 
